77f6ab09940731081a6f150d29172cb17739d03c
王乐 <lewang0454@foxmail.com>
2023-10-31 20:01:21 +0800

    重构了“参加招募页”的样式（尚未美化），正在实现过滤功能

--- frontend/src/views/JoinRecruitmentPage.vue
+++ frontend/src/views/JoinRecruitmentPage.vue
@@ -12,33 +12,63 @@
         </div>
         <div class="content-container">
             <div class="table-container">
+                <h2 class="title">招募列表</h2>
                 <div class="recruitments-container">
-                    <el-table
-                        :data="this.displayedList" border
-                        :header-cell-style="{background:'#eef1f6', color:'#606266'}"
-                        @row-click="handleRowClick"
-                    >
-
-                        <el-table-column prop="status" label="状态" align="center" ></el-table-column>
-        
-                        <el-table-column prop="project" label="所属项目" align="center"></el-table-column>
-                        
-                        <el-table-column prop="date" label="招募时间" align="center"></el-table-column>
-
-                        <el-table-column prop="location" label="招募地点" align="center"></el-table-column>
-        
-                        <el-table-column prop="type" label="面向群体" align="center"></el-table-column>
-
-                        <el-table-column prop="hours" label="志愿时长" align="center"></el-table-column>
-        
-                        <el-table-column prop="totalNumber" label="招募人数" align="center"></el-table-column>   
-                               
-                    </el-table>
+                    <table class="table-style" border>
+                        <thead>
+                            <tr>
+                                <th style="background-color: #e8e8e4">状态</th>
+                                <th style="background-color: #e8e8e4">所属项目</th>
+                                <th style="background-color: #e8e8e4">招募时间</th>
+                                <th style="background-color: #e8e8e4">面向群体</th>
+                                <th style="background-color: #e8e8e4">招募地点</th>
+                                <th style="background-color: #e8e8e4">志愿时长</th>
+                                <th style="background-color: #e8e8e4">招募人数</th>
+                            </tr>
+                            <tr>
+                                <th style="width: 150px;">
+                                    <el-select v-model="status" clearable placeholder="选择招募状态">
+                                        <el-option key="招募中" value="招募中">招募中</el-option>
+                                        <el-option key="结束招募" value="结束招募">结束招募</el-option>
+                                    </el-select>
+                                </th>
+                                <th style="width: 150px;">
+                                    <el-input v-model="keyword" placeholder="搜索项目名称" clearable></el-input>
+                                </th>
+                                <th style="width: 200px;">
+                                    <el-date-picker
+                                    v-model="date"
+                                    type="date"
+                                    placeholder="搜索招募时间"
+                                    clearable
+                                ></el-date-picker>
+                                </th>
+                                <th style="width: 150px;">
+                                    <el-select v-model="type" placeholder="面向对象" clearable>
+                                        <el-option key="面向公共招募" value="面向公共招募">面向公共招募</el-option>
+                                        <el-option key="仅限团队内部" value="仅限团队内部">仅限团队内部</el-option>
+                                    </el-select>
+                                </th>
+                                <th colspan="3" style="width: 450px;"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in displayedList" @click="handleClickRow(item)">
+                                <td style="text-align:center">{{ item.status }}</td>
+                                <td style="text-align:center">{{ item.project }}</td>
+                                <td style="text-align:center">{{ item.date }}</td>
+                                <td style="text-align:center">{{ item.type }}</td>
+                                <td style="text-align:center">{{ item.location }}</td>
+                                <td style="text-align:center">{{ item.hours }} 小时</td>
+                                <td style="text-align:center">{{ item.number }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
                 </div>
                 <div class="pagination-container">
                     <el-pagination 
                         @current-change="handlePageChange"
-                        :page-size="15"
+                        :page-size="20"
                         :total="totalList.length"
                         layout="prev, pager, next">
                     </el-pagination>
@@ -54,47 +84,55 @@ export default {
     data() {
         return {
             currentPage: 1,
+            status: null,
+            keyword: null,
+            date: null,
+            type: null,
+            hours: null,
             totalList: [
                 {status: "招募中", project: "项目1", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
                 {status: "招募中", project: "项目2", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "结束招募", project: "项目3", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "招募中", project: "项目3", date: "2023-10-29 23:59", location: "田径场", type: "仅限团队内部", hours: 16, number: 5, totalNumber: 10},
                 {status: "招募中", project: "项目4", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
                 {status: "招募中", project: "项目5", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "结束招募", project: "项目6", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "招募中", project: "项目6", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
                 {status: "招募中", project: "项目7", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目8", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目8", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
                 {status: "结束招募", project: "项目9", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目10", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目11", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目10", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目11", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
                 {status: "结束招募", project: "项目12", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目13", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目13", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
                 {status: "结束招募", project: "项目14", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目15", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目16", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目15", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目16", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
                 {status: "结束招募", project: "项目17", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目18", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
-                {status: "招募中", project: "项目19", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目18", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目19", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目20", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目21", date: "2023-10-29 23:59", location: "田径场", type: "仅限团队内部", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目22", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目23", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
+                {status: "结束招募", project: "项目", date: "2023-10-29 23:59", location: "田径场", type: "面向公共", hours: 16, number: 5, totalNumber: 10},
             ],
             selectedRow: null,
         }
     },
     computed: {
         displayedList() {
-            let startIndex = (this.currentPage - 1) * 15;
-            return this.totalList.slice(startIndex, startIndex + 15);
+            let startIndex = (this.currentPage - 1) * 20;
+            return this.totalList.slice(startIndex, startIndex + 20);
+        },
+        filterdList() {
+            
         }
     },
     methods: {
         getStatusClass(status) {
             return status === '招募中' ? "green-border" : "red-border";
         },
-        handleRowClick(row) {
-            this.selectedRow = row;
-            console.log("click!!!");
-            console.log(this.selectedRow.status, 
-                this.selectedRow.project, this.selectedRow.date, 
-                this.selectedRow.location, this.selectedRow.type, 
-                this.selectedRow.hours, this.selectedRow.number);
+        handleClickRow(row) {
+            console.log(row.status, row.project, row.date, row.location, row.type, row.hours, row.number);
         },
         handlePageChange(currentPage) {
             this.currentPage = currentPage;
@@ -145,19 +183,32 @@ export default {
     flex-direction: column;
 }
 
+.title {
+    margin-top: 30px;
+}
+
+.table-style {
+    border-collapse: collapse;
+}
+
 .recruitments-container {
     display: flex;
     flex-wrap: wrap;
     justify-content: flex-start;
     align-content: flex-start;
     margin-top: 30px;
-    margin-left: 30px;
+    margin-left: 130px;
     height: 700px;
     width: 1350px;
 }
 
+.table-container {
+    width: 100%;
+}
+
 .pagination-container {
     margin-top: 50px;
+    margin-bottom: 50px;
 }
 
 .green-border {
