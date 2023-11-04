edab131b5e494ff93885078b62a47f456aa129ef
Zhang Haoxiang <singleApo@outlook.com>
2023-11-05 00:18:00 +0800

    userPage

--- frontend/src/views/UserPage.vue
+++ frontend/src/views/UserPage.vue
@@ -1,142 +1,128 @@
 <template>
-    <el-radio-group v-model="size">
-      <el-radio label="large">Large</el-radio>
-      <el-radio>Default</el-radio>
-      <el-radio label="small">Small</el-radio>
-    </el-radio-group>
-  
-    <el-descriptions
-      class="margin-top"
-      title="With border"
-      :column="3"
-      :size="size"
-      border
-    >
-      <template #extra>
-        <el-button type="primary">Operation</el-button>
+  <el-radio-group v-model="size">
+    <el-radio label="large">Large</el-radio>
+    <el-radio>Default</el-radio>
+    <el-radio label="small">Small</el-radio>
+  </el-radio-group>
+
+  <el-descriptions class="margin-top" :column="3" :size="size" border>
+    <template #extra>
+    </template>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          <el-icon :style="iconStyle">
+            <User />
+          </el-icon>
+          collegeId
+        </div>
       </template>
-      <el-descriptions-item>
-        <template #label>
-          <div class="cell-item">
-            <el-icon :style="iconStyle">
-              <user />
-            </el-icon>
-            Username
-          </div>
-        </template>
-        kooriookami
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template #label>
-          <div class="cell-item">
-            <el-icon :style="iconStyle">
-              <iphone />
-            </el-icon>
-            Telephone
-          </div>
-        </template>
-        18100000000
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template #label>
-          <div class="cell-item">
-            <el-icon :style="iconStyle">
-              <location />
-            </el-icon>
-            Place
-          </div>
-        </template>
-        Suzhou
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template #label>
-          <div class="cell-item">
-            <el-icon :style="iconStyle">
-              <tickets />
-            </el-icon>
-            Remarks
-          </div>
-        </template>
-        <el-tag size="small">School</el-tag>
-      </el-descriptions-item>
-      <el-descriptions-item>
-        <template #label>
-          <div class="cell-item">
-            <el-icon :style="iconStyle">
-              <office-building />
-            </el-icon>
-            Address
-          </div>
-        </template>
-        No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
-      </el-descriptions-item>
-    </el-descriptions>
-  
-    <el-descriptions
-      class="margin-top"
-      title="Without border"
-      :column="3"
-      :size="size"
-      :style="blockMargin"
-    >
-      <template #extra>
-        <el-button type="primary">Operation</el-button>
+      21371295
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          <el-icon :style="iconStyle">
+            <User />
+          </el-icon>
+          userName
+        </div>
       </template>
-      <el-descriptions-item label="Username">kooriookami</el-descriptions-item>
-      <el-descriptions-item label="Telephone">18100000000</el-descriptions-item>
-      <el-descriptions-item label="Place">Suzhou</el-descriptions-item>
-      <el-descriptions-item label="Remarks">
-        <el-tag size="small">School</el-tag>
-      </el-descriptions-item>
-      <el-descriptions-item label="Address"
-        >No.1188, Wuzhong Avenue, Wuzhong District, Suzhou, Jiangsu Province
-      </el-descriptions-item>
-    </el-descriptions>
-  </template>
+      张昊翔
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          <el-icon :style="iconStyle">
+            <Iphone />
+          </el-icon>
+          telephone
+        </div>
+      </template>
+      18100000000
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          <el-icon :style="iconStyle">
+            <VideoCamera />
+          </el-icon>
+          email
+        </div>
+      </template>
+      212345131@gmail.com
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          <el-icon :style="iconStyle">
+            <Tickets />
+          </el-icon>
+          userType
+        </div>
+      </template>
+      <el-tag size="small">普通用户</el-tag>
+    </el-descriptions-item>
+    <el-descriptions-item>
+      <template #label>
+        <div class="cell-item">
+          <el-icon :style="iconStyle">
+            <DataBoard />
+          </el-icon>
+          userIntro
+        </div>
+      </template>
+      adadwdadaw
+    </el-descriptions-item>
+  </el-descriptions>
+
+</template>
   
-  <script setup lang="ts">
-  import { computed, ref } from 'vue'
-  import {
-    Iphone,
-    Location,
-    OfficeBuilding,
-    Tickets,
-    User,
-  } from '@element-plus/icons-vue'
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+import {
+  Iphone,
+  VideoCamera,
+  Tickets,
+  User,
+  DataBoard,
+} from '@element-plus/icons-vue'
+
+const size = ref('')
+const iconStyle = computed(() => {
+  const marginMap = {
+    large: '8px',
+    default: '6px',
+    small: '4px',
+  }
+  return {
+    marginRight: marginMap[size.value] || marginMap.default,
+  }
+})
+const blockMargin = computed(() => {
+  const marginMap = {
+    large: '32px',
+    default: '28px',
+    small: '24px',
+  }
+  return {
+    marginTop: marginMap[size.value] || marginMap.default,
+  }
+})
+</script>
   
-  const size = ref('')
-  const iconStyle = computed(() => {
-    const marginMap = {
-      large: '8px',
-      default: '6px',
-      small: '4px',
-    }
-    return {
-      marginRight: marginMap[size.value] || marginMap.default,
-    }
-  })
-  const blockMargin = computed(() => {
-    const marginMap = {
-      large: '32px',
-      default: '28px',
-      small: '24px',
-    }
-    return {
-      marginTop: marginMap[size.value] || marginMap.default,
-    }
-  })
-  </script>
-  
-  <style scoped>
-  .el-descriptions {
-    margin-top: 20px;
-  }
-  .cell-item {
-    display: flex;
-    align-items: center;
-  }
-  .margin-top {
-    margin-top: 20px;
-  }
-  </style>
+<style scoped>
+.el-descriptions {
+  margin-top: 20px;
+}
+
+.cell-item {
+  display: flex;
+  align-items: center;
+}
+
+.margin-top {
+  margin-top: 20px;
+}
+</style>
   
